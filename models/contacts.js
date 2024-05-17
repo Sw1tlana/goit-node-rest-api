dbc4c5a7994b08a8be9b9554abe6b99c5c78ad5a
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-05-17 09:55:21 +0300

    Changes DB_URI, added foulder models file contacts.js

---
+++ models/contacts.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const contactsSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  
+})
